97c0e16b30388542ad454bba77669b8987821089
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2022-01-08 20:06:11 +0530

    Smoothened controller movements for snake using Euler angles so that it turns to the relevant direction

--- Snake2D/Assets/Scripts/PlayerController.cs
+++ Snake2D/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
- 
+    Vector3 originalAngles;
+    private void Awake()
+    {
+        originalAngles = gameObject.transform.eulerAngles;
+    }
+
+    private void Start()
+    {
+        //Debug.Log("Test statements on Start");
+    }
+
     public Rigidbody2D rigidbody2d;
     public float speed;
     //public GameObject player;
@@ -13,20 +23,29 @@ public class PlayerController : MonoBehaviour
  
     void Update()
     {
-        if(Input.GetAxis("Horizontal")>0) //positive
+        
+        if (Input.GetKeyDown(KeyCode.D)) //positive
         {
+            transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
+            transform.localRotation = Quaternion.Euler(0, 0, -90);
             rigidbody2d.velocity = new Vector2(speed, 0f);
         }
-        else if(Input.GetAxis("Horizontal")<0) //negative
+        else if(Input.GetKeyDown(KeyCode.A)) //negative
         {
+            transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
             rigidbody2d.velocity = new Vector2(-speed, 0f);
         }
-        else if(Input.GetAxis("Vertical")>0) //positive
+        else if(Input.GetKeyDown(KeyCode.W)) //positive
         {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             rigidbody2d.velocity = new Vector2(0f, speed);
         }
-        else if(Input.GetAxis("Vertical")<0) //negative
+        else if (Input.GetKeyDown(KeyCode.S))  //negative
         {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            transform.localScale = new Vector3(transform.localScale.x, (Mathf.Abs(transform.localScale.y)*-1), transform.localScale.z);
             rigidbody2d.velocity = new Vector2(0f, -speed);
         }
     }
