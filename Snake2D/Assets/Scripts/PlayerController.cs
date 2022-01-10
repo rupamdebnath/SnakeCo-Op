f7ae1939ee3e010568dfbd55c42ab49596504fed
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2022-01-10 18:01:56 +0530

    fixed the issue that snake shouldn't be able to move in opposite direction using bool variables in PlayerController.cs

--- Snake2D/Assets/Scripts/PlayerController.cs
+++ Snake2D/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
 
     private float Xpos, Ypos;
 
+    private bool canMoveRight, canMoveLeft, canMoveUp, canMoveDown = true;
+
     private FoodController _foodcontroller;
       
     private void Start()
@@ -24,42 +26,59 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //for (int i = snakeSegments.Count - 1; i > 0; i--)
+        //{
+        //    snakeSegments[i].position = new Vector3((snakeSegments[i - 1].position.x + Xpos), (snakeSegments[i - 1].position.y + Ypos), snakeSegments[i - 1].position.z);
+        //}
+
         for (int i = snakeSegments.Count - 1; i > 0; i--)
         {
-            snakeSegments[i].position = new Vector3((snakeSegments[i - 1].position.x + Xpos), (snakeSegments[i - 1].position.y + Ypos), snakeSegments[i - 1].position.z);
+            snakeSegments[i].position = snakeSegments[i-1].position;
         }
 
-        if (Input.GetKeyDown(KeyCode.D)) //positive
+            if (Input.GetKeyDown(KeyCode.D) && canMoveRight) //positive
         {
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             transform.localRotation = Quaternion.Euler(0, 0, -90);
             rigidbody2d.velocity = new Vector2(speed, 0f);
             Xpos = -0.4f;
             Ypos = 0;
+            canMoveLeft = false;
+            canMoveUp = true;
+            canMoveDown = true;
         }
-        else if(Input.GetKeyDown(KeyCode.A)) //negative
+        else if(Input.GetKeyDown(KeyCode.A) && canMoveLeft) //negative
         {
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             transform.localRotation =  Quaternion.Euler(0, 0, 90);
             rigidbody2d.velocity = new Vector2(-speed, 0f);
             Xpos = 0.4f;
             Ypos = 0;
+            canMoveRight = false;
+            canMoveUp = true;
+            canMoveDown = true;
         }
-        else if(Input.GetKeyDown(KeyCode.W)) //positive
+        else if(Input.GetKeyDown(KeyCode.W) && canMoveUp) //positive
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             rigidbody2d.velocity = new Vector2(0f, speed);
             Ypos = -0.4f;
             Xpos = 0;
+            canMoveDown = false;
+            canMoveLeft = true;
+            canMoveRight = true;
         }
-        else if (Input.GetKeyDown(KeyCode.S))  //negative
+        else if (Input.GetKeyDown(KeyCode.S) && canMoveDown)  //negative
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
             transform.localScale = new Vector3(transform.localScale.x, (Mathf.Abs(transform.localScale.y)*-1), transform.localScale.z);
             rigidbody2d.velocity = new Vector2(0f, -speed);
             Ypos = 0.4f;
             Xpos = 0;
+            canMoveUp = false;
+            canMoveLeft = true;
+            canMoveRight = true;
         }        
     }
 
@@ -79,15 +98,12 @@ public class PlayerController : MonoBehaviour
     private void Grow()
     {
         Transform segment = Instantiate(this.snakeBodyPrefab);
-        segment.position = snakeSegments[snakeSegments.Count - 1].position;
+        //segment.position = snakeSegments[snakeSegments.Count - 1].position;
 
+        //segment.position = new Vector3((snakeSegments[snakeSegments.Count - 1].position.x + Xpos), (snakeSegments[snakeSegments.Count - 1].position.y + Ypos), snakeSegments[snakeSegments.Count - 1].position.z);
+        segment.position = new Vector3(0, 0, 0);
         snakeSegments.Add(segment);
         Debug.Log(snakeSegments.Count);
-    }
-
-    private void ReturnPosition()
-    {
-
     }
 
 }
