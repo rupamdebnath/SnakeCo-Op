9d08d0d822504c71b3789333b990bc2d7ee4734d
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2022-01-10 17:07:38 +0530

    Added body segment to generate on colliding with food

--- Snake2D/Assets/Scripts/PlayerController.cs
+++ Snake2D/Assets/Scripts/PlayerController.cs
@@ -4,50 +4,60 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
-    public GameObject snakeBody;
-    Vector3 originalAngles;
-    private void Awake()
-    {
-        originalAngles = gameObject.transform.eulerAngles;
-    }
+    private List<Transform> snakeSegments;
 
+    public Transform snakeBodyPrefab;
+
+    private float Xpos, Ypos;
+      
     private void Start()
     {
-        //Debug.Log("Test statements on Start");
+        snakeSegments = new List<Transform>();
+        snakeSegments.Add(this.transform);
+        Debug.Log(snakeSegments.Count);
     }
 
     public Rigidbody2D rigidbody2d;
-    public float speed;
-    //public GameObject player;
+    public float speed;    
 
-    private bool isGameOver = false;
- 
     void Update()
     {
-        
+        for (int i = snakeSegments.Count - 1; i > 0; i--)
+        {
+            snakeSegments[i].position = new Vector3((snakeSegments[i - 1].position.x + Xpos), (snakeSegments[i - 1].position.y + Ypos), snakeSegments[i - 1].position.z);
+        }
+
         if (Input.GetKeyDown(KeyCode.D)) //positive
         {
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             transform.localRotation = Quaternion.Euler(0, 0, -90);
             rigidbody2d.velocity = new Vector2(speed, 0f);
+            Xpos = -0.4f;
+            Ypos = 0;
         }
         else if(Input.GetKeyDown(KeyCode.A)) //negative
         {
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
-            transform.localRotation = Quaternion.Euler(0, 0, 90);
+            transform.localRotation =  Quaternion.Euler(0, 0, 90);
             rigidbody2d.velocity = new Vector2(-speed, 0f);
+            Xpos = 0.4f;
+            Ypos = 0;
         }
         else if(Input.GetKeyDown(KeyCode.W)) //positive
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
             transform.localScale = new Vector3(transform.localScale.x, Mathf.Abs(transform.localScale.y), transform.localScale.z);
             rigidbody2d.velocity = new Vector2(0f, speed);
+            Ypos = -0.4f;
+            Xpos = 0;
         }
         else if (Input.GetKeyDown(KeyCode.S))  //negative
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
             transform.localScale = new Vector3(transform.localScale.x, (Mathf.Abs(transform.localScale.y)*-1), transform.localScale.z);
             rigidbody2d.velocity = new Vector2(0f, -speed);
+            Ypos = 0.4f;
+            Xpos = 0;
         }        
     }
 
@@ -56,7 +66,24 @@ public class PlayerController : MonoBehaviour
         if (collision.tag == "Apple")
         {
             Destroy(collision.gameObject);
+            Grow();
+
+            //Instantiate(snakeBodyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         }
+    }
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.snakeBodyPrefab);
+        segment.position = snakeSegments[snakeSegments.Count - 1].position;
+
+        snakeSegments.Add(segment);
+        Debug.Log(snakeSegments.Count);
+
+    }
+
+    private void ReturnPosition()
+    {
+
     }
 
 }
