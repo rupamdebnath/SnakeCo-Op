5bdfa98c9838e416324a557f62b49b9efa766f48
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-02-16 22:32:44 +0530

    Added sounds, Sound Manager singleton script, background music is working fine

--- Snake2D/Assets/Scripts/SoundManager.cs
+++ Snake2D/Assets/Scripts/SoundManager.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioSource SoundSfx;
+
+    public AudioSource SoundBGMusic;
+
+    public UISoundType[] sounds;
+
+    //Singleton script for SoundManager Instance
+    private static SoundManager instance;
+    public static SoundManager Instance { get { return instance; } }
+
+    //Awake
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        KeepPlaying(UISounds.GameBGMusic);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void KeepPlaying(UISounds _sound)
     {
-        
+        AudioClip clip = getSoundClip(_sound);
+        if (clip != null)
+        {
+            SoundBGMusic.clip = clip;
+            SoundBGMusic.Play();
+        }
+        else
+        {
+            Debug.Log("No clip found for Sound Type");
+        }
+    }
+
+    private AudioClip getSoundClip(UISounds sound)
+    {
+        UISoundType _soundtype = Array.Find(sounds, s => s.soundType == sound);
+        if (_soundtype != null)
+            return _soundtype.soundClip;
+        return null;
+    }
+
+    [Serializable]
+    public class UISoundType
+    {
+        public UISounds soundType;
+        public AudioClip soundClip;
+    }
+    public enum UISounds
+    {
+        ButtonClick,        
+        GameBGMusic,
+        FoodPickup,
+        GameOver,
+        LevelWin
     }
 }
