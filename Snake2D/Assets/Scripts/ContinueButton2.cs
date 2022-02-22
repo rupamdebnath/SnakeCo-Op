2ef944d81752fd7f4dc7adbc26824f5c3df6f468
Rupam Debnath <44625252+rupamdebnath@users.noreply.github.com>
2022-02-22 15:49:13 +0530

    Added Instructions UI Canvas for controls with continue button

---
+++ Snake2D/Assets/Scripts/ContinueButton2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButton2 : MonoBehaviour
+{
+    public GameObject InstructionsUI;
+
+    public GameObject InstructionsUI2;
+
+    private Button button;
+
+    //Awake
+    void Awake()
+    {
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        InstructionsUI.SetActive(false);
+        InstructionsUI2.SetActive(true);
+    }
+}
